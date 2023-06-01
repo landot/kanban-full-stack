f60f9ef9c06f328fae353e9fba3a61ca0bf2185f
landot <timowland@gmail.com>
2023-06-01 13:46:39 -0700

    fix imports in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,80 +4,14 @@ import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { ThemeContext, ThemeContextType } from './context/ThemeContext';
 import { Column } from './components/Column';
-import { Task } from '../features/task/Task';
 import { KanbanTest } from '../features/kanban/KanbanTest';
-import { Status, Column as IColumn } from './types/data';
+import { Column as IColumn } from './types/data';
+import { sampleBoard } from './data/sampleData';
 import './App.css'
 
 function App() {
   const [theme, setTheme] = useState<ThemeContextType>(localStorage.getItem('theme') as ThemeContextType  || 'light');
-  const [boardData, setBoardData] = useState<IColumn[]>([
-    {
-      name: 'Column1',
-      id: 'col1', 
-      color: '#49C4E5',
-      tasks: [
-        {
-            id: '5555',
-            title: 'Build UI for onboarding',
-            description: 'this is the description for the task asdfasdfasdfasfd',
-            status: Status.Todo,
-            subtasks: [
-                {
-                    id: '123456',
-                    title: 'create design for UI',
-                    isCompleted: true
-                },
-                {
-                    id: '123457',
-                    title: 'develop MVP',
-                    isCompleted: false
-                },
-                {
-                    id: '123458',
-                    title: 'test MVP',
-                    isCompleted: false
-                }
-            ]
-        },
-        {
-            id: '6666',
-            title: 'Test new UI',
-            description: 'this is the description for the task asdfasdfasdfasfd',
-            status: Status.Todo,
-            subtasks: [
-                {
-                    id: '1123456',
-                    title: 'accessibility testing',
-                    isCompleted: true
-                },
-                {
-                    id: '1123457',
-                    title: 'performance testing',
-                    isCompleted: false
-                },
-                {
-                    id: '1123458',
-                    title: 'sign off from QA',
-                    isCompleted: false
-                }
-            ]
-        }
-    ]
-    },
-    {
-      name: 'Column2',
-      id: 'col2', 
-      color: '#8471F2',
-      tasks: []
-    },
-    {
-      name: 'Column3',
-      id: 'col3', 
-      color: '#67E2AE',
-      tasks: []
-    }
-  ]);
+  const [boardData, setBoardData] = useState<IColumn[]>(sampleBoard.columns);
   console.log(boardData);
 
   function toggleTheme() {
@@ -108,7 +42,6 @@ function App() {
         <Sidebar boards={['Test1', 'Test2']} selectedBoardIndex={0} handleToggleTheme={toggleTheme}/>
         <Header boardName={'Test1'} />
         <div className='content'>
-          <Task />
           <KanbanTest />
           {/* <DragDropContext onDragEnd={handleDragEnd}>
             <Column name={boardData[0].name} id={boardData[0].id} tasks={boardData[0].tasks} color={boardData[0].color} />
